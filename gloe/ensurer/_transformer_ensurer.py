334df859f22e021b0978b949ea51fe1cad61fbda
venkatkrishna <venkatkrishna@moe>
2025-02-11 03:08:44 +0100

    Reset to d9e8fce3fd0a05435b82a5f09c0af03f6ff40505 and made changes for LLM Round 1

--- gloe/ensurer/_transformer_ensurer.py
+++ gloe/ensurer/_transformer_ensurer.py
@@ -1,62 +1,62 @@
 import inspect
 from abc import abstractmethod, ABC
-from types import FunctionType
-from typing import Any, Callable, Generic, ParamSpec, Sequence, TypeVar, cast, overload
+from typing import Any, Callable, Generic, Sequence, TypeVar, cast
 
 from gloe.async_transformer import AsyncTransformer
 from gloe.functional import _PartialTransformer, _PartialAsyncTransformer
 from gloe.transformers import Transformer
 
-_T = TypeVar("_T")
-_S = TypeVar("_S")
-_U = TypeVar("_U")
-_P1 = ParamSpec("_P1")
+T = TypeVar("T")
+S = TypeVar("S")
+U = TypeVar("U")
 
 
-class TransformerEnsurer(Generic[_T, _S], ABC):
+class TransformerEnsurer(Generic[T, S], ABC):
     @abstractmethod
-    def validate_input(self, data: _T):
-        """Perform a validation on incoming data before execute the transformer code"""
+    def validate_input(self, data: T):
+        """Validate incoming data before executing the transformer."""
 
     @abstractmethod
-    def validate_output(self, data: _T, output: _S):
-        """Perform a validation on outcome data after execute the transformer code"""
+    def validate_output(self, data: T, output: S):
+        """Validate output data after executing the transformer."""
+
+    def __call__(self, transformer: Transformer[T, S]) -> Transformer[T, S]:
+        def transform(this: Transformer, data: T) -> S:
+            self.validate_input(data)
+            try:
+                output = transformer.transform(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during transformation: {e}")
+            self.validate_output(data, output)
+            return output
+
+        transformer_cp = transformer.copy(transform)
+        return transformer_cp
 
 
-def input_ensurer(func: Callable[[_T], Any]) -> TransformerEnsurer[_T, Any]:
-    class LambdaEnsurer(TransformerEnsurer[_T, _S]):
+def input_ensurer(func: Callable[[T], Any]) -> TransformerEnsurer[T, Any]:
+    class LambdaEnsurer(TransformerEnsurer[T, S]):
         __doc__ = func.__doc__
-        __annotations__ = cast(FunctionType, func).__annotations__
 
-        def validate_input(self, data: _T):
+        def validate_input(self, data: T):
             func(data)
 
-        def validate_output(self, data: _T, output: _S):
+        def validate_output(self, data: T, output: S):
             pass
 
     return LambdaEnsurer()
 
 
-@overload
-def output_ensurer(func: Callable[[_T, _S], Any]) -> TransformerEnsurer[_T, _S]:
-    pass
-
-
-@overload
-def output_ensurer(func: Callable[[_S], Any]) -> TransformerEnsurer[Any, _S]:
-    pass
-
-
-def output_ensurer(func: Callable):
+def output_ensurer(func: Callable) -> TransformerEnsurer:
     class LambdaEnsurer(TransformerEnsurer):
         __doc__ = func.__doc__
-        __annotations__ = cast(FunctionType, func).__annotations__
 
         def validate_input(self, data):
             pass
 
         def validate_output(self, data, output):
-            if len(inspect.signature(func).parameters) == 1:
+            sig = inspect.signature(func)
+            if len(sig.parameters) == 1:
                 func(output)
             else:
                 func(data, output)
@@ -65,26 +65,6 @@ def output_ensurer(func: Callable):
 
 
 class _ensure_base:
-    @overload
-    def __call__(self, transformer: Transformer[_U, _S]) -> Transformer[_U, _S]:
-        pass
-
-    @overload
-    def __call__(
-        self, transformer_init: _PartialTransformer[_T, _P1, _U]
-    ) -> _PartialTransformer[_T, _P1, _U]:
-        pass
-
-    @overload
-    def __call__(self, transformer: AsyncTransformer[_U, _S]) -> AsyncTransformer[_U, _S]:
-        pass
-
-    @overload
-    def __call__(
-        self, transformer_init: _PartialAsyncTransformer[_T, _P1, _U]
-    ) -> _PartialAsyncTransformer[_T, _P1, _U]:
-        pass
-
     def __call__(self, arg):
         if isinstance(arg, Transformer):
             return self._generate_new_transformer(arg)
@@ -118,15 +98,18 @@ class _ensure_base:
         pass
 
 
-class _ensure_incoming(Generic[_T], _ensure_base):
-    def __init__(self, incoming: Sequence[Callable[[_T], Any]]):
-        self.input_ensurers_instances = [input_ensurer(ensurer) for ensurer in incoming]
+class _ensure_incoming(Generic[T], _ensure_base):
+    def __init__(self, incoming: Sequence[Callable[[T], Any]]):
+        self.input_ensurers = [input_ensurer(ensurer) for ensurer in incoming]
 
     def _generate_new_transformer(self, transformer: Transformer) -> Transformer:
-        def transform(_, data):
-            for ensurer in self.input_ensurers_instances:
+        def transform(_, data: T) -> S:
+            for ensurer in self.input_ensurers:
                 ensurer.validate_input(data)
-            output = transformer.transform(data)
+            try:
+                output = transformer.transform(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during transformation: {e}")
             return output
 
         transformer_cp = transformer.copy(transform)
@@ -135,103 +118,106 @@ class _ensure_incoming(Generic[_T], _ensure_base):
     def _generate_new_async_transformer(
         self, transformer: AsyncTransformer
     ) -> AsyncTransformer:
-        async def transform_async(_, data):
-            for ensurer in self.input_ensurers_instances:
+        async def transform_async(_, data: T) -> S:
+            for ensurer in self.input_ensurers:
                 ensurer.validate_input(data)
-            output = await transformer.transform_async(data)
+            try:
+                output = await transformer.transform_async(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during asynchronous transformation: {e}")
             return output
 
         transformer_cp = transformer.copy(transform_async)
-
         return transformer_cp
 
 
-class _ensure_outcome(Generic[_S], _ensure_base):
-    def __init__(self, incoming: Sequence[Callable[[_S], Any]]):
-        self.output_ensurers_instances = [output_ensurer(ensurer) for ensurer in incoming]
+class _ensure_outcome(Generic[S], _ensure_base):
+    def __init__(self, outcome: Sequence[Callable[[S], Any]]):
+        self.output_ensurers = [output_ensurer(ensurer) for ensurer in outcome]
 
     def _generate_new_transformer(self, transformer: Transformer) -> Transformer:
-        def transform(_, data):
-            output = transformer.transform(data)
-            for ensurer in self.output_ensurers_instances:
+        def transform(_, data: T) -> S:
+            try:
+                output = transformer.transform(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during transformation: {e}")
+            for ensurer in self.output_ensurers:
                 ensurer.validate_output(data, output)
             return output
 
         transformer_cp = transformer.copy(transform)
-
         return transformer_cp
 
     def _generate_new_async_transformer(
         self, transformer: AsyncTransformer
     ) -> AsyncTransformer:
-        async def transform_async(_, data):
-            output = await transformer.transform_async(data)
-            for ensurer in self.output_ensurers_instances:
+        async def transform_async(_, data: T) -> S:
+            try:
+                output = await transformer.transform_async(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during asynchronous transformation: {e}")
+            for ensurer in self.output_ensurers:
                 ensurer.validate_output(data, output)
             return output
 
         transformer_cp = transformer.copy(transform_async)
-
         return transformer_cp
 
 
-class _ensure_changes(Generic[_T, _S], _ensure_base):
-    def __init__(self, changes: Sequence[Callable[[_T, _S], Any]]):
-        self.changes_ensurers_instances = [output_ensurer(ensurer) for ensurer in changes]
+class _ensure_changes(Generic[T, S], _ensure_base):
+    def __init__(self, changes: Sequence[Callable[[T, S], Any]]):
+        self.changes_ensurers = [output_ensurer(ensurer) for ensurer in changes]
 
     def _generate_new_transformer(self, transformer: Transformer) -> Transformer:
-        def transform(_, data):
-            output = transformer.transform(data)
-            for ensurer in self.changes_ensurers_instances:
+        def transform(_, data: T) -> S:
+            try:
+                output = transformer.transform(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during transformation: {e}")
+            for ensurer in self.changes_ensurers:
                 ensurer.validate_output(data, output)
             return output
 
         transformer_cp = transformer.copy(transform)
-
         return transformer_cp
 
     def _generate_new_async_transformer(
         self, transformer: AsyncTransformer
     ) -> AsyncTransformer:
-        async def transform_async(_, data):
-            output = await transformer.transform_async(data)
-            for ensurer in self.changes_ensurers_instances:
+        async def transform_async(_, data: T) -> S:
+            try:
+                output = await transformer.transform_async(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during asynchronous transformation: {e}")
+            for ensurer in self.changes_ensurers:
                 ensurer.validate_output(data, output)
             return output
 
         transformer_cp = transformer.copy(transform_async)
-
         return transformer_cp
 
 
-class _ensure_both(Generic[_T, _S], _ensure_base):
+class _ensure_both(Generic[T, S], _ensure_base):
     def __init__(
         self,
-        incoming: Sequence[Callable[[_T], Any]],
-        outcome: Sequence[Callable[[_S], Any]],
-        changes: Sequence[Callable[[_T, _S], Any]],
+        incoming: Sequence[Callable[[T], Any]],
+        outcome: Sequence[Callable[[S], Any]],
+        changes: Sequence[Callable[[T, S], Any]],
     ):
-        incoming_seq = incoming if type(incoming) == list else [incoming]
-        self.input_ensurers_instances = [
-            input_ensurer(ensurer) for ensurer in incoming_seq
-        ]
-
-        outcome_seq = outcome if type(outcome) == list else [outcome]
-        self.output_ensurers_instances = [
-            output_ensurer(ensurer) for ensurer in outcome_seq
-        ]
-
-        changes_seq = changes if type(changes) == list else [changes]
-        self.output_ensurers_instances = self.output_ensurers_instances + [
-            output_ensurer(ensurer) for ensurer in changes_seq
-        ]
+        self.input_ensurers = [input_ensurer(ensurer) for ensurer in incoming]
+        self.output_ensurers = [output_ensurer(ensurer) for ensurer in outcome]
+        self.changes_ensurers = [output_ensurer(ensurer) for ensurer in changes]
+        self.output_ensurers.extend(self.changes_ensurers)
 
     def _generate_new_transformer(self, transformer: Transformer) -> Transformer:
-        def transform(_, data):
-            for ensurer in self.input_ensurers_instances:
+        def transform(_, data: T) -> S:
+            for ensurer in self.input_ensurers:
                 ensurer.validate_input(data)
-            output = transformer.transform(data)
-            for ensurer in self.output_ensurers_instances:
+            try:
+                output = transformer.transform(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during transformation: {e}")
+            for ensurer in self.output_ensurers:
                 ensurer.validate_output(data, output)
             return output
 
@@ -241,115 +227,41 @@ class _ensure_both(Generic[_T, _S], _ensure_base):
     def _generate_new_async_transformer(
         self, transformer: AsyncTransformer
     ) -> AsyncTransformer:
-        async def transform_async(_, data):
-            for ensurer in self.input_ensurers_instances:
+        async def transform_async(_, data: T) -> S:
+            for ensurer in self.input_ensurers:
                 ensurer.validate_input(data)
-            output = await transformer.transform_async(data)
-            for ensurer in self.output_ensurers_instances:
+            try:
+                output = await transformer.transform_async(data)
+            except Exception as e:
+                raise RuntimeError(f"Error during asynchronous transformation: {e}")
+            for ensurer in self.output_ensurers:
                 ensurer.validate_output(data, output)
             return output
 
         transformer_cp = transformer.copy(transform_async)
         return transformer_cp
-
-
-@overload
-def ensure(incoming: Sequence[Callable[[_T], Any]]) -> _ensure_incoming[_T]:
-    pass
-
-
-@overload
-def ensure(outcome: Sequence[Callable[[_S], Any]]) -> _ensure_outcome[_S]:
-    pass
-
-
-@overload
-def ensure(changes: Sequence[Callable[[_T, _S], Any]]) -> _ensure_changes[_T, _S]:
-    pass
-
-
-@overload
-def ensure(
-    incoming: Sequence[Callable[[_T], Any]], outcome: Sequence[Callable[[_S], Any]]
-) -> _ensure_both[_T, _S]:
-    pass
-
-
-@overload
-def ensure(
-    incoming: Sequence[Callable[[_T], Any]], changes: Sequence[Callable[[_T, _S], Any]]
-) -> _ensure_both[_T, _S]:
-    pass
-
-
-@overload
-def ensure(
-    outcome: Sequence[Callable[[_T], Any]], changes: Sequence[Callable[[_T, _S], Any]]
-) -> _ensure_both[_T, _S]:
-    pass
-
-
-@overload
-def ensure(
-    incoming: Sequence[Callable[[_T], Any]],
-    outcome: Sequence[Callable[[_S], Any]],
-    changes: Sequence[Callable[[_T, _S], Any]],
-) -> _ensure_both[_T, _S]:
-    pass
 
 
 def ensure(*args, **kwargs):
     """
-    This decorator is used in transformers to ensure some validation based on its incoming
-    data, outcome data, or both.
-
-    These validations are performed by validators. Validators are simple callable
-    functions that validate certain aspects of the input, output, or the differences
-    between them. If the validation fails, it must raise an exception.
-
-    The decorator :code:`@ensure` returns some intermediate classes to assist with the
-    internal logic of Gloe. However, the result of applying it to a transformer is just
-    a new transformer with the exact same attributes, but it includes an additional
-    validation layer.
-
-    The motivation of the many overloads is just to allow the user to define different types
-    of validators interchangeably.
-
-    See also:
-        For more detailed information about this feature, refer to the :ref:`ensurers` page.
+    Decorator to add validation layers to transformers based on incoming, outcome, or both data.
 
     Args:
-        incoming (Sequence[Callable[[_T], Any]]): sequence of validators that will be
-            applied to the incoming data. The type :code:`_T` refers to the incoming type.
-            Defaut value: :code:`[]`.
-        outcome (Sequence[Callable[[_S], Any]]): sequence of validators that will be
-            applied to the outcome data. The type :code:`_S` refers to the outcome type.
-            Defaut value: :code:`[]`.
-        changes (Sequence[Callable[[_T, _S], Any]]): sequence of validators that will be
-            applied to both incoming and outcome data. The type :code:`_T` refers to the
-            incoming type, and type :code:`_S` refers to the outcome type.
-            Defaut value: :code:`[]`.
+        incoming (Sequence[Callable[[T], Any]]): Validators for incoming data.
+        outcome (Sequence[Callable[[S], Any]]): Validators for outcome data.
+        changes (Sequence[Callable[[T, S], Any]]): Validators for both incoming and outcome data.
     """
-    if len(kwargs.keys()) == 1 and "incoming" in kwargs:
+    if "incoming" in kwargs:
         return _ensure_incoming(kwargs["incoming"])
 
-    if len(kwargs.keys()) == 1 and "outcome" in kwargs:
+    if "outcome" in kwargs:
         return _ensure_outcome(kwargs["outcome"])
 
-    if len(kwargs.keys()) == 1 and "changes" in kwargs:
+    if "changes" in kwargs:
         return _ensure_changes(kwargs["changes"])
 
-    if len(kwargs.keys()) > 1:
-        incoming = []
-        if "incoming" in kwargs:
-            incoming = kwargs["incoming"]
-
-        outcome = []
-        if "outcome" in kwargs:
-            outcome = kwargs["outcome"]
-
-        changes = []
-        if "changes" in kwargs:
-            changes = kwargs["changes"]
-
+    if len(kwargs) > 1:
+        incoming = kwargs.get("incoming", [])
+        outcome = kwargs.get("outcome", [])
+        changes = kwargs.get("changes", [])
         return _ensure_both(incoming, outcome, changes)
